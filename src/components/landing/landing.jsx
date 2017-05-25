24e78857c00c3bf83304015471e72922b342a403
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-05-25 11:52:03 -0500

    flexbox-like styling for landing page

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,13 +1,17 @@
 import {Link} from 'react-router-dom'
-import {Container, Header} from 'semantic-ui-react'
+import {Grid, Header} from 'semantic-ui-react'
 
 class Landing extends React.Component {
     render() {
         return (
-            <Container>
-                <Header size="large">Landing</Header>
-                <Link to="/about">About</Link>
-            </Container>
+            <Grid columns={3} height="100%" className="max-height">
+                <Grid.Row verticalAlign="middle" centered>
+                    <Grid.Column textAlign="center" className="center-column" width={6}>
+                        <Header size="large">Landing</Header>
+                        <Link to="/about">About</Link>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         )
     }
 }
