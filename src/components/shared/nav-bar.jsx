d9410388382fb39a8f47a5ed8071349033bf35d4
Eric Moore <moore.ericc@gmail.com>
2017-05-25 18:16:00 -0500

    menu items

--- src/components/shared/nav-bar.jsx
+++ src/components/shared/nav-bar.jsx
@@ -18,9 +18,21 @@ class NavBar extends React.Component {
     render() {
         const {activeItem} = this.state
         return (
-            <Menu attached="top">
-                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} as={Link} to="/"/>
-                <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} as={Link} to="/about"/>
+            <Menu attached="top" pointing secondary>
+                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} as={Link} to="/">
+                    Home
+                </Menu.Item>
+                <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} as={Link} to="/about">
+                    About
+                </Menu.Item>
+                <Menu.Menu position='right'>
+                    <Menu.Item name='signup' active={activeItem === 'signup'} onClick={this.handleItemClick}>
+                        Sign Up
+                    </Menu.Item>
+                    <Menu.Item name='help' active={activeItem === 'help'} onClick={this.handleItemClick}>
+                        Help
+                    </Menu.Item>
+                </Menu.Menu>
             </Menu>
         )
     }
