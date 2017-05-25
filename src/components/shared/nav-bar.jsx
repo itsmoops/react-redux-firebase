2dbc17ec6639af66e0759b21c4da5888e0e7fcb3
Eric Moore <moore.ericc@gmail.com>
2017-05-25 18:06:51 -0500

    trying new menu

--- src/components/shared/nav-bar.jsx
+++ src/components/shared/nav-bar.jsx
@@ -1,10 +1,27 @@
-import {Segment} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+import {Menu} from 'semantic-ui-react'
 import './nav-bar.less'
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            activeItem: 'home'
+        }
+        this.handleItemClick = this.handleItemClick.bind(this)
+    }
+
+    handleItemClick(e, {name}) {
+        this.setState({activeItem: name})
+    }
+
     render() {
+        const {activeItem} = this.state
         return (
-            <Segment inverted attached="top" textAlign="center">React | Redux | Webpack</Segment>
+            <Menu attached="top">
+                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} as={Link} to="/"/>
+                <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} as={Link} to="/about"/>
+            </Menu>
         )
     }
 }
