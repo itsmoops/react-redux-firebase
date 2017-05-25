c3ecec5e01e981985c57abce4cbc0c35abfaa0f9
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-05-25 15:32:10 -0500

    removing css calc tests

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -4,7 +4,7 @@ import {Grid, Header} from 'semantic-ui-react'
 class HomePage extends React.Component {
     render() {
         return (
-            <Grid columns={3} className="max-height-minus-nav">
+            <Grid columns={3} className="max-height">
                 <Grid.Row verticalAlign="middle" centered>
                     <Grid.Column textAlign="center" width={6}>
                         <Header size="large">Home</Header>
