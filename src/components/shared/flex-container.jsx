74cc8494e01e77fe7d31aac850945208105c26d1
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-05-26 09:23:20 -0500

    make flex container smarter

--- src/components/shared/flex-container.jsx
+++ src/components/shared/flex-container.jsx
@@ -1,15 +1,25 @@
 import {Grid, Segment} from 'semantic-ui-react'
 
 class FlexContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.gutterSize = this.gutterSize.bind(this)
+    }
+    gutterSize(size) {
+        return (16 - size) / 2
+    }
     render() {
+        const computerSize = (this.props.computer && parseInt(this.props.computer)) || 6
+        const tabletSize = (this.props.tablet && parseInt(this.props.tablet)) || 10
+        const mobileSize = (this.props.tablet && parseInt(this.props.tablet)) || 14
         return (
             <Grid columns={3} className="max-height">
                 <Grid.Row verticalAlign="middle" centered>
-                    <Grid.Column computer={5} tablet={4} mobile={2}/>
-                    <Grid.Column computer={6} tablet={8} mobile={12} textAlign="center">
+                    <Grid.Column computer={this.gutterSize(computerSize)} tablet={this.gutterSize(tabletSize)} mobile={this.gutterSize(mobileSize)}/>
+                    <Grid.Column computer={computerSize} tablet={tabletSize} mobile={mobileSize} textAlign="center">
                         {this.props.children}
                     </Grid.Column>
-                    <Grid.Column computer={5} tablet={4} mobile={2}/>
+                    <Grid.Column computer={this.gutterSize(computerSize)} tablet={this.gutterSize(tabletSize)} mobile={this.gutterSize(mobileSize)}/>
                 </Grid.Row>
             </Grid>
         )
