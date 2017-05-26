387256ea18f3d9838c1b1ec9693793c73a9189a8
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-05-26 10:16:34 -0500

    i’m an idiot

--- src/components/shared/flex-container.jsx
+++ src/components/shared/flex-container.jsx
@@ -1,26 +1,16 @@
-import {Grid, Segment} from 'semantic-ui-react'
+import {Grid} from 'semantic-ui-react'
 
+/**
+ * FlexContainer - Semantic UI grid wrapper that builds a responsive flexbox
+ */
 class FlexContainer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.gutterSize = this.gutterSize.bind(this)
-    }
-    gutterSize(size) {
-        // semantic UI has a 16 column grid
-        return (16 - size) / 2
-    }
     render() {
-        const computerSize = (this.props.computer && parseInt(this.props.computer)) || 6
-        const tabletSize = (this.props.tablet && parseInt(this.props.tablet)) || 10
-        const mobileSize = (this.props.mobile && parseInt(this.props.mobile)) || 14
         return (
             <Grid columns={3} className="max-height">
                 <Grid.Row verticalAlign="middle" centered>
-                    <Grid.Column computer={this.gutterSize(this.props.computer)} tablet={this.gutterSize(this.props.tablet)} mobile={this.gutterSize(this.props.mobile)}/>
                     <Grid.Column computer={this.props.computer} tablet={this.props.tablet} mobile={this.props.mobile} textAlign="center">
                         {this.props.children}
                     </Grid.Column>
-                    <Grid.Column computer={this.gutterSize(this.props.computer)} tablet={this.gutterSize(this.props.tablet)} mobile={this.gutterSize(this.props.mobile)}/>
                 </Grid.Row>
             </Grid>
         )
