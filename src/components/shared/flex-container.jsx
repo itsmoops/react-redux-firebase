5874a6fdc68603fdf5b0bc1d0438a58aa4e8919b
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-05-26 09:00:26 -0500

    responsive flex box

--- src/components/shared/flex-container.jsx
+++ src/components/shared/flex-container.jsx
@@ -1,13 +1,15 @@
-import {Grid} from 'semantic-ui-react'
+import {Grid, Segment} from 'semantic-ui-react'
 
 class FlexContainer extends React.Component {
     render() {
         return (
             <Grid columns={3} className="max-height">
                 <Grid.Row verticalAlign="middle" centered>
-                    <Grid.Column textAlign="center">
+                    <Grid.Column computer={5} tablet={4} mobile={2}/>
+                    <Grid.Column computer={6} tablet={8} mobile={12} textAlign="center">
                         {this.props.children}
                     </Grid.Column>
+                    <Grid.Column computer={5} tablet={4} mobile={2}/>
                 </Grid.Row>
             </Grid>
         )
