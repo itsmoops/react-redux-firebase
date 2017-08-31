74abf49f9307e789c2139f60d6ed58f17cdaad3c
Eric Moore <Eric.Moore2@bestbuy.com>
2017-08-31 09:29:36 -0500

    mobile detection using regex first

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,6 +1,12 @@
 const utilities = {
     isSmallDevice() {
-        return window.innerWidth < 768
+        const mobileReg = new RegExp(/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/)
+        if (mobileReg.test(window.navigator.userAgent)) {
+            return true
+        } else if (window.innerWidth < 768) {
+            return true
+        }
+        return false
     }
 }
 
