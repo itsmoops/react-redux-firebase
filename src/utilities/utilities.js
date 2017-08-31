7f28452c7c21305c551c360a1f4b78b87a57f67c
Eric Moore <Eric.Moore2@bestbuy.com>
2017-08-31 09:48:58 -0500

    use shorter regex

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,6 +1,6 @@
 const utilities = {
     isSmallDevice() {
-        const mobileReg = new RegExp(/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/)
+        const mobileReg = new RegExp(/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile/)
         if (mobileReg.test(window.navigator.userAgent)) {
             return true
         } else if (window.innerWidth < 768) {
