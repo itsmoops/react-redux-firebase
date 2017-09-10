0a0689a8a7a33faff89fc966b2ee30a192b451a7
Eric Moore <moore.ericc@gmail.com>
2017-09-10 14:02:19 -0500

    server

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const http = require('http')
 
-http.createServer(() => {}).listen(process.env.PORT || 8000)
+const port = process.env.PORT || 8000
+
+http.createServer(() => {}).listen(port)
+
+console.log(`Server running at http://127.0.0.1:${port}/`)
