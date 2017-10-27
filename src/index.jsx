4454f64871f0eec096c57ab84dc03e83a49f04fe
Eric Moore <moore.ericc@gmail.com>
2017-10-27 18:55:12 -0500

    Revert "remove firebase - we doing this django thing"
    This reverts commit 616b1f8362a9750f33ed42748e51d2cc6582d769.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
+import * as firebase from 'firebase'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import routes from './routes'
 import configureStore from './store/config'
+import config from './firebase-config'
 import './app.less'
+
+firebase.initializeApp(config)
 
 // can pass our initialState here - useful for server rendering
 const store = configureStore()
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,8 @@ module.exports = {
             ReactDOM: 'react-dom',
             PropTypes: 'prop-types',
             Icon: ['react-icons-kit', 'Icon'],
-            horizontalCenter: ['react-icons-kit', 'horizontalCenter']
+            horizontalCenter: ['react-icons-kit', 'horizontalCenter'],
+            firebase: 'firebase'
         }),
         new ExtractTextPlugin('[name].css')
     ],
