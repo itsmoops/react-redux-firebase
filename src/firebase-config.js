dc58a9fa976e4326b1a035351b6efbe14987877c
Eric Moore <moore.ericc@gmail.com>
2017-10-28 16:06:31 -0500

    add prod database for testing

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,7 +8,14 @@ const firebaseConfig = {
         messagingSenderId: '823691206640'
     },
 
-    prod: {}
+    prod: {
+        apiKey: 'AIzaSyDZS2SRPGP6gHxvfl3RpdWgW3jn9KYn8cc',
+        authDomain: 'boilerplate-prod-b5875.firebaseapp.com',
+        databaseURL: 'https://boilerplate-prod-b5875.firebaseio.com',
+        projectId: 'boilerplate-prod-b5875',
+        storageBucket: '',
+        messagingSenderId: '708307577611'
+    }
 }
 
 export default firebaseConfig
