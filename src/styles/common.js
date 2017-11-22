efd2c20c821f09dce669e560620bcdb05eb9033f
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-11-22 14:18:34 -0600

    add encoding

--- src/styles/common.js
+++ src/styles/common.js
@@ -7,7 +7,8 @@ const common = {
     screen: {
         large: '@media screen and (min-width: 64em)',
         medium: '@media screen and (min-width: 52em)',
-        small: '@media screen and (min-width: 40em)'
+        small: '@media screen and (min-width: 40em)',
+        xSmall: '@media screen and (max-width: 39.99em)'
     },
     disabled: `cursor: initial;
                 filter: alpha(opacity=65); opacity: 0.65;
